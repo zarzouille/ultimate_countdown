46035aad7f6f9bff64d4e3f11750b38a20beb92b
Xavier <xavier.collongues@gmail.com>
2026-02-10 11:38:58 +0100

    Change default port from 5000 to 5001

--- app.py
+++ app.py
@@ -360,5 +360,5 @@ def countdown_image(countdown_id):
 # ============================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
